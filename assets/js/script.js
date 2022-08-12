c6a2645959d94ed60f1d1684b1aaa907f063d885
Minotaurius <brandonslanger@gmail.com>
2022-08-11 21:28:14 -0500

    API is now fetching curr day and 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,12 +35,34 @@ btn.addEventListener('click', function(event){
             // console.log(data)
             var currCityLon = data[0].lon;
             var currCityLat = data[0].lat;
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${currCityLat}&lon=${currCityLon}&appid=813d1ce96b3d7fd78ff4cce567bf840d`)
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${currCityLat}&lon=${currCityLon}&units=imperial&appid=813d1ce96b3d7fd78ff4cce567bf840d`)
         .then(function(res) {
             return res.json()
         })
         .then(function(data) {
             console.log(data)
+        $('#curr-temp').text('Temperature: ' + data.current.temp + 'F')
+        $('#curr-wind').text('Wind: ' + data.current.wind_speed + 'mph')
+        $('#curr-hum').text('Humidity: ' + data.current.humidity + '%')
+        $('#curr-uv').text('Current UV Index: ' + data.current.uvi)
+        $('#day1-temp').text('Temperature: ' + data.daily[0].temp.max + 'F')
+        $('#day1-wind').text('Wind: ' + data.daily[0].wind_speed + 'mph')
+        $('#day1-hum').text('Humidity: ' + data.daily[0].humidity + '%')
+        $('#day2-temp').text('Temperature: ' + data.daily[1].temp.max + 'F')
+        $('#day2-wind').text('Wind: ' + data.daily[1].wind_speed + 'mph')
+        $('#day2-hum').text('Humidity: ' + data.daily[1].humidity + '%')
+        $('#day3-temp').text('Temperature: ' + data.daily[2].temp.max + 'F')
+        $('#day3-wind').text('Wind: ' + data.daily[2].wind_speed + 'mph')
+        $('#day3-hum').text('Humidity: ' + data.daily[2].humidity + '%')
+        $('#day4-temp').text('Temperature: ' + data.daily[2].temp.max + 'F')
+        $('#day4-wind').text('Wind: ' + data.daily[3].wind_speed + 'mph')
+        $('#day4-hum').text('Humidity: ' + data.daily[3].humidity + '%')
+        $('#day5-temp').text('Temperature: ' + data.daily[4].temp.max + 'F')
+        $('#day5-wind').text('Wind: ' + data.daily[4].wind_speed + 'mph')
+        $('#day5-hum').text('Humidity: ' + data.daily[4].humidity + '%')
+        })
+        .then(function(data) {
+
         })
         })
 });
